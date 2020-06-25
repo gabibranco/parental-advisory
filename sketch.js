9c68ec58ea450b53fe4b67ff67a8363c7477763f
gabibranco <gabi.branco@gmail.com>
2020-06-25 06:20:51 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,59 @@
+let font, x, y,
+  fontsize = 20;
+
+function preload() {
+  // Ensure the .ttf or .otf font stored in the assets directory
+  // is loaded before setup() and draw() are called
+  font = 'Asset';
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  // Set text characteristics
+  textFont(font);
+  textSize(fontsize);
+  textAlign(CENTER, CENTER);
+  x = width/2
+  y = height
+}
+
+function draw() {
+  background(0);
+  fill(65);
+  text('at this very moment,', x, y);
+  fill(105)
+  text('a black young person', x, y + 70)
+  fill(120);
+  text('is most likely being', x, y + 140)
+  fill(170);
+  text('violently killed in Brazil.', x, y + 210);
+  fill(210);
+  text('by the time you go to bed,', x, y + 280);
+  fill(255);
+  text('62 will have died today.', x, y + 350);
+  
+  y = y - 1;
+  
+  // Reset to the bottom
+  if (y < -140) {
+    y = height;
+  }
+}
+
+function drawWords(x) {
+  // The text() function needs three parameters:
+  // the text to draw, the horizontal position,
+  // and the vertical position
+  fill(40);
+  text('ichi', x, 80);
+
+  fill(65);
+  text('ni', x, 150);
+
+  fill(190);
+  text('san', x, 220);
+
+  fill(255);
+  text('shi', x, 290);
+}
